bb5ef8a8caec287a04f3d980401081a6b1c233bc
Amberdg <127230506+Amberdg@users.noreply.github.com>
2023-04-04 11:38:52 +0200

    Wall in EscapeFactory

--- src/main/java/com/example/demo/EntityTypes.java
+++ src/main/java/com/example/demo/EntityTypes.java
@@ -1,5 +1,5 @@
 package com.example.demo;
 
 public enum EntityTypes {
-    PLAYER, GUARD, KEY
+    PLAYER, GUARD, KEY, WALL
 }
--- src/main/java/com/example/demo/EscapeFactory.java
+++ src/main/java/com/example/demo/EscapeFactory.java
@@ -6,19 +6,31 @@ import com.almasb.fxgl.entity.EntityFactory;
 import com.almasb.fxgl.entity.SpawnData;
 import com.almasb.fxgl.entity.Spawns;
 import com.almasb.fxgl.entity.components.CollidableComponent;
+import com.almasb.fxgl.physics.BoundingShape;
+import com.almasb.fxgl.physics.HitBox;
+import com.almasb.fxgl.physics.PhysicsComponent;
 
 import static com.almasb.fxgl.dsl.FXGLForKtKt.entityBuilder;
 
 public class EscapeFactory implements EntityFactory {
 
     @Spawns("player")
-    public Entity newBat(SpawnData data) {
+    public Entity newPlayer(SpawnData data) {
         return entityBuilder(data)
 //                .at(100, 100)
                 .viewWithBBox("test2.png")
                 .with(new CollidableComponent(true))
                 .scale(0.2, 0.2)
                 .type(EntityTypes.PLAYER)
+                .build();
+    }
+
+    @Spawns("wall")
+    public Entity newWall(SpawnData data){
+        return entityBuilder(data)
+                .type(EntityTypes.WALL)
+                .bbox(new HitBox(BoundingShape.box(data.<Integer>get("width"), data.<Integer>get("height"))))
+                .with(new PhysicsComponent())
                 .build();
     }
 
