f0621ac6b29f100f52bdae7124a6610b02899a6a
MonkeSingh <129729790+MonkeSingh@users.noreply.github.com>
2023-04-04 15:22:20 +0200

    Delete old files directory

--- old files/src/main/resources/assets/levels/tiles_spritesheet.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.0" name="tiles_spritesheet" tilewidth="25" tileheight="25" tilecount="1332" columns="36">
- <image source="tiles_spritesheet.png" width="914" height="936"/>
-</tileset>
--- old files/target/classes/assets/levels/tiles_spritesheet1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.0" name="tiles_spritesheet1" tilewidth="70" tileheight="70" tilecount="169" columns="13">
- <image source="tiles_spritesheet1.png" width="914" height="936"/>
-</tileset>
